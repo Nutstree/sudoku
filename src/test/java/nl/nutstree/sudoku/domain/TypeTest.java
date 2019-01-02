383453bdc4aae2acff9dec6fe6d3fe4776fccfc1
Chris <chris@nutstree.nl>
2019-01-02 14:56:39 +0200

    - made interfaces of sudoku equal for both implementations

--- src/test/java/nl/nutstree/sudoku/domain/TypeTest.java
+++ src/test/java/nl/nutstree/sudoku/domain/TypeTest.java
@@ -1,6 +1,5 @@
-package nl.nutstree.sudoku.domain.immutables;
+package nl.nutstree.sudoku.domain;
 
-import nl.nutstree.sudoku.domain.Type;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
